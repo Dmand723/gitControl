b712b50d6e438a8a171954de8e813eb46778ed6e
Dmand723 <dawson.simmons723@gmail.com>
2024-12-03 13:56:44 -0700

    added a feature see README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GitControl
+
+<h2>Clicking c anywhere on the page will change the background to a random color between red, blue, green, and white pressing v andywhere will change the background color to a completely random color</h2>
---
+++ script.js
@@ -0,0 +1,26 @@
+const body = document.querySelector("body");
+const colors = {
+  0: {
+    rgb: "rgb(255,0,0)",
+  },
+  1: {
+    rgb: "rgb(0,255,0)",
+  },
+  2: {
+    rgb: "rgb(0,0,255)",
+  },
+  3: {
+    rgb: "rgb(255,255,255)",
+  },
+};
+document.addEventListener("keydown", (e) => {
+  if (e.key.toLowerCase() == "c") {
+    let rand = Math.round(Math.random() * 3);
+    body.style = `background-color: ${colors[rand].rgb};`;
+  } else if (e.key.toLowerCase() == "v") {
+    let randColor = `rgb(${Math.round(Math.random() * 255)},${Math.round(
+      Math.random() * 255
+    )},${Math.round(Math.random() * 255)})`;
+    body.style = `background-color: ${randColor};`;
+  }
+});
